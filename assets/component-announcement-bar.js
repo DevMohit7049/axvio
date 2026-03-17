c5bfcb8a7509ee060f340016d8d9a094e6861568
Mohit Tomar <tomarmohit7049@gmail.com>
2026-03-17 15:28:48 +0530

    update liquid code

--- assets/component-announcement-bar.js
+++ assets/component-announcement-bar.js
@@ -1,24 +1,29 @@
-document.addEventListener("DOMContentLoaded",function(){
-    const message = document.querySelectorAll('.message');
-    const leftArrow = document.querySelectorAll('.arrow .left');
-    const rightArrow = document.querySelectorAll('.arrow .right');
+document.addEventListener("DOMContentLoaded", function () {
+  const bars = document.querySelectorAll(".announcement-bar");
+
+  bars.forEach((bar) => {
+    const messages = bar.querySelectorAll(".message");
+    
+    const prev = bar.querySelector(".arrow.left");
+    const next = bar.querySelector(".arrow.right");
 
     let index = 0;
-    if(message.length===0) return;
 
-    function showMessage(i){
-        
-        message.forEach((msg)=>msg.classList.remove("active"));
-        message[i].classList.add("active");
+    if (messages.length === 0) return;
+
+    function showMessage(i) {
+      messages.forEach((msg) => msg.classList.remove("active"));
+      messages[i].classList.add("active");
     }
 
-    rightArrow?.addEventListener("click",()=>{
-        index = (index+1) % message.length;
-        showMessage(index);
+    next?.addEventListener("click", () => {
+      index = (index + 1) % messages.length;
+      showMessage(index);
     });
 
-        leftArrow?.addEventListener("click",()=>{
-        index = (index-1 + message.length) % message.length;
-        showMessage(index);
+    prev?.addEventListener("click", () => {
+      index = (index - 1 + messages.length) % messages.length;
+      showMessage(index);
     });
-})
+  });
+});
